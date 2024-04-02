30b88489349835a5886384d47a95af8c98b926c6
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-04-02 23:07:19 +0100

    test(observeFileEvents): rewrite test to avoid edge cases due to partially written sentinel

--- index.test.ts
+++ index.test.ts
@@ -2,10 +2,11 @@ import {open, rename, rm} from "node:fs/promises";
 import * as os from "node:os";
 import test from "ava";
 import {temporaryFileTask} from "tempy";
-import {filter, BehaviorSubject, EMPTY, firstValueFrom, interval, of, Subject} from "rxjs";
+import {EMPTY, firstValueFrom, interval, of, Subject} from "rxjs";
 import {
     concatWith,
     delay,
+    filter,
     map,
     mergeMap,
     mergeWith,
@@ -89,14 +90,38 @@ test("observeFileEvents", async t => {
     );
 });
 
-type TestFileState = "Init" | "SeenSentinel" | "Ready" | "Done";
+type InternalEvent =
+    | "WritingSentinel"
+    | "WroteSentinel"
+    | "SeenWritingSentinel"
+    | "SeenWrittenSentinel"
+    | "Ready"
+    | "Done"
+    | {readonly event: "Error"; readonly reason: unknown};
+
+type ScannedInternalEvent =
+    | "Init"
+    | "RequestSentinel"
+    | "WritingSentinel"
+    | "WroteSentinel"
+    | "SeenWritingSentinel"
+    | "SeenWrittenSentinel"
+    | "Ready"
+    | "Done";
+
+type InternalState =
+    | "Init"
+    | "WritingSentinel"
+    | "SeenWritingSentinel"
+    | "SeenWrittenSentinel"
+    | "Ready"
+    | "Done";
 
 async function testFileObservable(actions: (path: string) => Promise<void>): Promise<FileEvent[]> {
     return temporaryFileTask(async path => {
-        const stateEvents = new BehaviorSubject<TestFileState>("Init");
-        const errorEvents = new Subject<unknown>();
+        const requestSentinelEvents = interval(1).pipe(map(() => "RequestSentinel" as const));
+        const internalEvents = new Subject<InternalEvent>();
         const fileEvents = observeFileEvents(path);
-        const requestSentinel = interval(1).pipe(map(() => "RequestSentinel" as const));
 
         const writeEmptyFile = async (): Promise<void> => {
             const file = await open(path, "w");
@@ -107,41 +132,57 @@ async function testFileObservable(actions: (path: string) => Promise<void>): Pro
         await writeEmptyFile();
 
         return firstValueFrom(
-            requestSentinel
+            requestSentinelEvents
                 .pipe(
-                    takeUntil(stateEvents.pipe(filter(state => state === "SeenSentinel"))),
+                    takeUntil(
+                        internalEvents.pipe(
+                            filter(
+                                event =>
+                                    event === "SeenWritingSentinel" ||
+                                    event === "SeenWrittenSentinel"
+                            )
+                        )
+                    ),
                     mergeWith(
-                        stateEvents,
                         fileEvents,
-                        errorEvents.pipe(
-                            map(reason => {
-                                throw reason;
+                        internalEvents.pipe(
+                            map(event => {
+                                if (typeof event === "object") {
+                                    throw event.reason;
+                                } else {
+                                    return event;
+                                }
                             })
                         )
                     ),
                     scan(
                         (
                             previous: {
-                                readonly state: TestFileState;
-                                readonly event: FileEvent | TestFileState | "RequestSentinel";
+                                readonly event: ScannedInternalEvent | FileEvent;
+                                readonly state: InternalState;
                             },
                             event
                         ) =>
-                            ({
-                                event,
-                                state:
-                                    event === "SeenSentinel" ||
-                                    event === "Ready" ||
-                                    event === "Done"
-                                        ? event
-                                        : previous.state
-                            }) as const,
-                        {event: "Init", state: "Init"}
+                            event === "WritingSentinel" ||
+                            event === "SeenWritingSentinel" ||
+                            event === "SeenWrittenSentinel" ||
+                            event === "Ready" ||
+                            event === "Done"
+                                ? ({event, state: event} as const)
+                                : event === "WroteSentinel"
+                                  ? previous.state === "SeenWritingSentinel"
+                                      ? ({
+                                            event: "SeenWrittenSentinel",
+                                            state: "SeenWrittenSentinel"
+                                        } as const)
+                                      : ({event: "WroteSentinel", state: "Init"} as const)
+                                  : {event, state: previous.state},
+                        {event: "Init", state: "Init"} as const
                     ),
                     switchMap(({event, state}) =>
                         of({event, state}).pipe(
                             concatWith(
-                                event === "SeenSentinel"
+                                event === "SeenWrittenSentinel"
                                     ? of({event: "DelayAfterSeenSentinel", state} as const).pipe(
                                           delay(2)
                                       )
@@ -150,39 +191,44 @@ async function testFileObservable(actions: (path: string) => Promise<void>): Pro
                         )
                     ),
                     mergeMap(({event, state}) =>
-                        event === "Done"
-                            ? of({event, state}).pipe(delay(4))
-                            : of({
-                                  event,
-                                  state
-                              })
+                        event === "Done" ? of({event, state}).pipe(delay(4)) : of({event, state})
                     ),
                     tap(({event, state}) => {
                         if (event === "RequestSentinel") {
                             if (state === "Init") {
-                                void writeEmptyFile();
+                                internalEvents.next("WritingSentinel");
+                                void writeEmptyFile().then(
+                                    () => void internalEvents.next("WroteSentinel")
+                                );
                             }
                         } else if (event === "DelayAfterSeenSentinel") {
-                            if (state === "Init" || state === "SeenSentinel") {
-                                stateEvents.next("Ready");
+                            if (state === "SeenWrittenSentinel") {
+                                internalEvents.next("Ready");
                             }
                         } else if (event === "Ready") {
                             void actions(path).then(
-                                () => void stateEvents.next("Done"),
-                                reason => void errorEvents.next(reason)
+                                () => void internalEvents.next("Done"),
+                                (reason: unknown) =>
+                                    void internalEvents.next({event: "Error", reason})
                             );
-                        } else if (
-                            typeof event === "object" &&
-                            (state === "Init" || state === "SeenSentinel")
-                        ) {
-                            stateEvents.next("SeenSentinel");
+                        } else if (typeof event === "object") {
+                            if (state === "Init" || state === "SeenWrittenSentinel") {
+                                internalEvents.next("SeenWrittenSentinel");
+                            } else if (
+                                state === "WritingSentinel" ||
+                                state === "SeenWritingSentinel"
+                            ) {
+                                internalEvents.next("SeenWritingSentinel");
+                            }
                         }
-                    }),
+                    })
+                )
+                .pipe(
                     skipWhile(({event}) => event !== "Ready"),
                     takeWhile(({event}) => event !== "Done"),
-                    mergeMap(({event}) => (typeof event === "object" ? of(event) : EMPTY))
+                    mergeMap(({event}) => (typeof event === "object" ? of(event) : EMPTY)),
+                    toArray()
                 )
-                .pipe(toArray())
         );
     });
 }
