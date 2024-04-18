a1fd71452f8010dc5d3cd0034fd9d74c3a9c6c16
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-04-18 15:48:33 +0100

    test(fs.watch): give watcher time to emit events before aborting

--- events.test.ts
+++ events.test.ts
@@ -56,6 +56,9 @@ test("fs.watch: watch file, events start emitting immediately after return", asy
         watcher.addListener("error", error => void events.push({event: "Error", error}));
         const file2 = await open(path, "w");
         await file2.close();
+        await firstValueFrom(
+            of(null).pipe(delay(os.platform() === "win32" || os.platform() === "linux" ? 20 : 200))
+        );
         abortController.abort();
         t.deepEqual(events, [{event: "change", path: basename(path)}]);
     });
